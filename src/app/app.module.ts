10483da47b909c54004ee880ace926e533b1ea5d
sai1306 <chillalesaiganesh@gmail.com>
2024-02-26 19:44:37 +0530

    real another add

---
+++ src/app/app.module.ts
@@ -0,0 +1,57 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { AppComponent } from './app.component';
+import {AutocompleteLibModule} from 'angular-ng-autocomplete';
+import { CUSTOM_ELEMENTS_SCHEMA } from '@angular/compiler';
+import { DisruptsComponent } from './disrupts/disrupts.component';
+import { NavComponent } from './nav/nav.component';
+import { SiteComponent } from './site/site.component';
+import {HttpClientModule} from '@angular/common/http';
+import { AppRoutingModule } from './app-routing.module';
+import { IndexComponent } from './index/index.component';
+import { TitleCasePipe } from '@angular/common';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { DialogComponent } from './dialog/dialog.component'; // Import TitleCasePipe
+import { MatDialogModule } from '@angular/material/dialog';
+import { ReactiveFormsModule } from '@angular/forms';
+import { CaptchaJs } from "@solarwinter/captchajs";
+import { MatSnackBarModule } from '@angular/material/snack-bar';
+import { ToastrModule } from 'ngx-toastr';
+
+import { NgxCaptchaModule } from 'ngx-captcha';
+import { LoginComponent } from './login/login.component';
+import { FormsModule } from '@angular/forms';
+import { PanelComponent } from './panel/panel.component';
+import { ReportsComponent } from './reports/reports.component';
+import { NgxSpinnerModule } from 'ngx-spinner';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    DisruptsComponent,
+    NavComponent,
+    SiteComponent,
+    IndexComponent,
+    DialogComponent,
+    LoginComponent,
+    PanelComponent,
+    ReportsComponent,
+  ],
+  imports: [
+    ToastrModule,
+    NgxSpinnerModule,
+    NgxCaptchaModule,
+    ReactiveFormsModule,
+    MatDialogModule,
+    BrowserModule,
+    AutocompleteLibModule, 
+    HttpClientModule,
+    AppRoutingModule, 
+    MatSnackBarModule,
+    BrowserAnimationsModule,
+    FormsModule
+  ],
+  providers: [TitleCasePipe],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
